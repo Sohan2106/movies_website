e3d80a63604a95b06b86b73bfc9ad51734c5e34f
Sohan S P <spsohan2106@gmail.com>
2017-07-06 12:27:35 +0530

    Update movies_website.py

--- movies_website.py
+++ movies_website.py
@@ -3,15 +3,9 @@ import web
 
 toy_story = movies.Movie("Toy Story", "A story of a boy and his toys that comes to life", "http://www.impawards.com/1995/posters/toy_story_ver1.jpg", "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
-#print(toy_story.trailer_youtube_url)
-
 avatar = movies.Movie("Avatar", "A marine on an alien planet", "http://s3.foxmovies.com/foxmovies/production/films/18/images/posters/251-asset-page.jpg", "https://www.youtube.com/watch?v=5PSNL1qE6VY")
 
-#print(avatar.storyline)
-#avatar.show_trailer()
-
 captain_america = movies.Movie("Captain America", "Story of the first avenger", "https://images-na.ssl-images-amazon.com/images/I/91H7VE0pSvL._AC_UL320_SR210,320_.jpg", "https://www.youtube.com/watch?v=JerVrbLldXw")
-#captain_america.show_trailer()
 
 the_a_team = movies.Movie("The A Team", "A group of Iraq War vetarans who were falsely accused of murder and theft looks to clear their name", "https://i.ytimg.com/vi/QvAuAba4RH0/movieposter.jpg", "https://www.youtube.com/watch?v=exyzEFrmLuM")
 
